10a9ac7bcb358d508a23d356880392a62698d57a
zhirobyte <faizhanafi90@gmail.com>
2025-07-14 15:09:45 +0700

    day 1 init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+]
+
+}
+
